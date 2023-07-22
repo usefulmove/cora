0118f59014a7613286939cf293632870d531ab61
Duane Edmonds <duane.edmonds@gmail.com>
2023-07-22 10:42:48 -0700

    minor

--- src/evaluate.rkt
+++ src/evaluate.rkt
@@ -1,2 +1,11 @@
 #lang racket
 
+; sample S-expressions
+(define sexp "(+ 3 2)")
+(define sexp2 "(+ 5 (+ 3 2))")
+
+; parse for everything within outermost parentheses
+(regexp-replace #rx"\\((.*)\\)" sexp2 "\\1")  ; "+ 5 (+ 3 2)"
+
+
+
