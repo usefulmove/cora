3a71154bc5cf435891a6801f7cb11265a51edce5
Duane Edmonds <duane.edmonds@gmail.com>
2023-07-22 19:50:36 -0700

    edited evaluation code

--- src/evaluate.rkt
+++ src/evaluate.rkt
@@ -4,7 +4,6 @@
 ; string->sexp :: string -> list
 (define (string->sexp string)
     (read (open-input-string string)))
-
 ; sample s-expressions
 (define sexp (string->sexp "(+ 3 2)"))
 (define sexp2 (string->sexp "(+ 5 (+ 3 2))"))
@@ -16,10 +15,13 @@
     (and (not (null? obj))
          (not (pair? obj))))
 
+
 ; evaluate s-expression
 ; eval-sexp :: list -> any
 (define (eval-sexp sexp)
-    (cond ([(null? sexp) '()]
-           [])
-    )
+    (cond ([(null? sexp) sexp]
+           [(atom? sexp) TODO]  ; if atom then either known symbol, number, . . . OR ???
+           [(pair? sexp) TODO]  ; if pair then go into another cond based on car
+    ))
 )
+
