0a6434db8b7c637bed2861973d14d9e1a97f000e
Duane Edmonds <duane.edmonds@gmail.com>
2023-08-23 20:59:43 -0700

    ...

---
+++ src/epoch.el
@@ -0,0 +1,26 @@
+;;; epoch.el --- functional programming algorithms library -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Robert Duane Edmonds
+;;
+;; Author: Duane <dedmonds@G3>
+;; Maintainer: Duane <dedmonds@G3>
+;; Created: August 23, 2023
+;; Modified: August 23, 2023
+;; Version: 0.0.1
+;; Keywords: functional programming algorithms library
+;; Homepage: https://github.com/dedmonds/epoch
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description:  functional programming algorithms library
+;;
+;;; Code:
+
+(defun all? (lst)
+  )
+
+(provide 'epoch)
+;;; epoch.el ends here
