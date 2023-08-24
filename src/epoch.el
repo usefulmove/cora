527a71dd260dc55694fdc44bc5f3115436f844c7
Duane Edmonds <duane.edmonds@gmail.com>
2023-08-24 00:01:45 -0700

    ...

--- src/epoch.el
+++ src/epoch.el
@@ -1,6 +1,6 @@
-;;; epoch.el --- functional programming algorithms library -*- lexical-binding: t; -*-
+;;; epic.el --- functional programming algorithms library -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 2023 Robert Duane Edmonds
+;; Copyright (c) 2023 Robert Duane Edmonds
 ;;
 ;; Author: Duane <dedmonds@G3>
 ;; Maintainer: Duane <dedmonds@G3>
@@ -8,7 +8,7 @@
 ;; Modified: August 23, 2023
 ;; Version: 0.0.1
 ;; Keywords: functional programming algorithms library
-;; Homepage: https://github.com/dedmonds/epoch
+;; Homepage: https://github.com/dedmonds/epic
 ;; Package-Requires: ((emacs "24.3"))
 ;;
 ;; This file is not part of GNU Emacs.
@@ -19,8 +19,52 @@
 ;;
 ;;; Code:
 
-(defun all? (lst)
-  )
+; map :: (T -> U) -> [T] -> [U]
+(defun map (f lst)
+  (cond ((null lst) '()) ; end of list?
+        (t (cons (funcall f (car lst))
+                 (map f (cdr lst))))))
 
-(provide 'epoch)
+(defun cube (n) (* n n n))
+(map 'cube (list 8 1 2))
+
+
+; curry2 :: (T -> U -> V) -> (T -> (U -> V))
+(defun curry2 (f)
+  (lambda (a) (lambda (b) (funcall f a b))))
+
+(defun add (a b) (+ a b))
+(add 3 2)
+
+(map (lambda (a) (add 3 a)) '(3 1 2))
+
+(setq curried-add (curry2 'add))
+
+(setq add3 (funcall curried-add 3))
+(funcall add3 2)
+
+(funcall (funcall curried-add 3) 2)
+
+
+(setq asdf 'map)
+(funcall asdf 'cube '(8 1 2))
+
+
+;(defun all? (lst)
+;  )
+;
+;(defun any? (lst)
+;  )
+;
+;(defun init (lst)
+;  )
+;
+;(defun prod (lst)
+;  )
+;
+;(defun sum (lst)
+;  )
+
+(provide 'epic)
 ;;; epoch.el ends here
+;;;
