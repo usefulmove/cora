24890880c24ae0d7dca8dff8607034d8668150eb
Duane Edmonds <duane.edmonds@gmail.com>
2023-08-26 23:16:07 -0700

    ...

--- src/epic.el
+++ src/epic.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Duane <dedmonds@gmail.com>
 ;; Created: August 23, 2023
 ;; Modified: August 26, 2023
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Keywords: extensions internal lisp tools
 ;; Homepage: https://github.com/dedmonds/epic
 ;; Package-Requires: ((emacs "24.3"))
@@ -21,109 +21,111 @@
 
 
 ;; map :: (T -> U) -> [T] -> [U]
-(defun map (f lst)
+(defun _map (f lst)
   "map function over elements of list"
   (cond ((null lst) '()) ; end of list?
         (t (cons (funcall f (car lst))
-                 (map f (cdr lst))))))
+                 (_map f (cdr lst))))))
+
+ ; built-in as mapcar
 
 
 ;; fold :: (T -> U -> T) -> [U] -> T
-(defun fold (f acc lst)
+(defun _fold (f acc lst)
   "fold (reduce) list"
   (cond ((null lst) acc)
-        (t (fold f (funcall f acc (car lst)) (cdr lst)))))
+        (t (_fold f (funcall f acc (car lst)) (cdr lst)))))
 
 
 ;; filter :: (T -> boolean) -> [T]
-(defun filter (f lst)
+(defun _filter (f lst)
   (cond ((null lst) '())
-        ((not (funcall f (car lst))) (filter f (cdr lst)))
+        ((not (funcall f (car lst))) (_filter f (cdr lst)))
         (t (cons (car lst)
-                 (filter f (cdr lst))))))
+                 (_filter f (cdr lst))))))
 
 
 ;; curry2 :: (T -> U -> V) -> (T -> (U -> V))
-(defun curry2 (f) ; TODO - troubleshoot
+(defun _curry2 (f) ; TODO - fixme
   "return curried binary function"
   (lambda (a)
     (lambda (b) (funcall f a b))))
 
 
 ;; range :: number -> [number]
-(defun range (n)
+(defun _range (n)
   (cond ((= 0 n) '())
-        (t (append (range (- n 1))
+        (t (append (_range (- n 1))
                    (list (- n 1))))))
 
 
 ;; inc :: number -> number
-(defun inc (n)
+(defun _inc (n)
   "increment number"
   (+ 1 n))
 
 
 ;; dec :: number -> number
-(defun dec (n)
+(defun _dec (n)
   "decrement number"
   (- n 1))
 
 
 ;; even? :: number -> boolean
-(defun even? (n)
+(defun _even? (n)
   (= 0 (mod n 2)))
 
 
 ;; odd? :: number -> boolean
-(defun odd? (n)
+(defun _odd? (n)
   (= 1 (mod n 2)))
 
 
 ;; zero? :: number -> boolean
-(defun zero? (n)
+(defun _zero? (n)
   (= 0 n))
 
 
 ;; sum :: [T] -> T
-(defun sum (lst)
+(defun _sum (lst)
   (apply '+ lst))
 
 
 ;; prod :: [T] -> T
-(defun prod (lst)
+(defun _prod (lst)
   (apply '* lst))
 
 
 ;; all? :: (T -> boolean) -> [T] -> boolean
-(defun all? (f lst)
+(defun _all? (f lst)
   "check that function applied to all values in the list returns true"
   (cond ((null lst) t)
         ((not (funcall f (car lst))) nil)
-        (t (all? f (cdr lst)))))
+        (t (_all? f (cdr lst)))))
 
 
 ;; any? :: (T -> boolean) -> [T] -> boolean
-(defun any? (f lst)
+(defun _any? (f lst)
   "check that function applied to at least one value in the list returns true"
   (cond ((null lst) nil)
         ((funcall f (car lst)) t)
-        (t (any? f (cdr lst)))))
+        (t (_any? f (cdr lst)))))
 
 
 ;; init :: [T] -> [T]
-(defun init (lst)
+(defun _init (lst)
   "return all elements of list except first"
   (reverse (cdr (reverse lst))))
 
 
 ;; last :: [T] -> [T]
-(defun last (lst)
+(defun _last (lst)
   "return the last element of the list"
   (car (reverse lst)))
 
 
 ; join-chars :: [char] -> string
-(defun join-chars (chars)
+(defun _join-chars (chars)
   (apply 'string chars))
 
 
